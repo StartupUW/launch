051bcbc80d7b8b099b211246fae7d6a53eb13f89
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-04-26 21:23:17 -0700

    Added the user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+	Schema = mongoose.Schema;
+	bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new Schema({
+	fname: String,
+	lname: String,
+	email: String,
+	bio: String,
+	major: String,
+	gradyr: Number,
+	links: [{
+		label: String,
+		url: String,
+		visible: Boolean
+	}]
+	username: {type: String, required: true, index: { unique: true }},
+	password: { type: String, required: true, select: false }
+});
+
+UserSchema.pre('save', function(next) {
+	var user = this;
+	if(!user.isModified('password')) return next();
+
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		if (err) return next(err);
+		user.password = hash;
+		next();
+	});
+
+});
+
+UserSchema.methods.comparePassword = function(password) {
+	var user = this;
+	return bcrypt.compareSync(password, user.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
