147f021585ff9709918c7788997e574106e3da35
Ayush <ayush29f@uw.edu>
2015-05-02 12:15:16 -0700

    some changes in models

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Projects = require('../models/projects');
+
 router.get('/', function(req, res, next) {
   res.send('User profiles');
+});
+
+router.get('/create', function(req, res){
+	var project = new Projects();
+
+
+
+
+	res.send('Welcome, submit a new project');
 });
 
 module.exports = router;
