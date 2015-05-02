876ae198f7b48bfbb3bf87154327ac2ea4b8f072
Daniel Fang <danfang@uw.edu>
2015-05-02 18:28:08 +0000

    Updating separate routes

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 var users = require('./routes/users');
+var admin = require('./routes/admin');
+
 var fs = require('fs');
 var app = express();
 
@@ -22,18 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-
-app.post('/myaction', function(req, res) {
-    res.send("DONE");
-    fs.appendFile("tmp/test", req.body.message, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-});
+app.use('/', index);
+app.use('/profile', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('User profiles');
+  res.send('Admin portal');
 });
 
 module.exports = router;
