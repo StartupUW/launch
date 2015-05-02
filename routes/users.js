ea0fd8261390ce7a8eee234a6da685c2dd36a3cc
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-05-02 13:16:40 -0700

    Modified Users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Users = require('./models/users');
+var Users = require('../models/users');
 // Models
 var Projects = require('../models/projects');
 
@@ -10,7 +10,18 @@ router.get('/', function(req, res, next) {
 
 router.route('/users')
 	.post(function(req,res) {
-
-	})
+		//Users.findById(req.params.user_id, function(err, user) {
+		//	if(err) {
+		//		res.send(err);
+		//	} else {
+				user.fname = req.body.fname;
+				user.lname = req.body.lname;
+				user.email = req.body.email;
+				user.bio = req.body.bio;
+				user.major = req.body.bio;
+				user.gradyr = req.body.gradyr;
+				res.json({message: "User Created!"});
+		//	}		
+	});
 
 module.exports = router;
