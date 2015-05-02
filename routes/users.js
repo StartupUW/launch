39ca9a05bbdedc9cf696a3063f1e0f74b56abc14
Ayush <ayush29f@uw.edu>
2015-05-02 14:35:12 -0700

    Setted up project routes

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,16 @@ router.get('/', function(req, res, next) {
   res.send('User profiles');
 });
 
-router.post('/create', function(req, res){
+router.get('/project/:pid', function(req, res){
+	Projects.findById(req.params.pid, function(err, project){
+		if(err) {
+			res.send(err);
+		}
+		res.render('project', { project : project });
+	});
+});
+
+router.post('/projects/create', function(req, res){
 	var project = new Projects();
 	project.name = req.body.name;
 	project.website = req.body.website;
@@ -16,6 +25,7 @@ router.post('/create', function(req, res){
 	project.contact.email = req.body.email;
 	project.contact.phone = req.body.phone;
 	project.type = req.body.type;
+	project.tags = req.body.tags;
 
 	project.save(function(err){
 		if(err) {
@@ -25,4 +35,25 @@ router.post('/create', function(req, res){
 	});
 });
 
+router.put('/project/:pid/update', function(req, res) {
+	Projects.findById(req.params.pid, function(err, project){
+		if(err) {
+			res.send(err);
+		}
+		project.name = req.body.name || project.name;
+		project.website = req.body.website || project.website;
+		project.description = req.body.desc || project.description;
+		project.contact.email = req.body.email || project.contact.email;
+		project.contact.phone = req.body.phone || project.contact.phone;
+		project.type = req.body.type || project.type;
+		project.tags = req.body.tags || project.tags;
+
+		project.save(function(err){
+			if(err) {
+				res.send(err);
+			}
+			res.json(project);
+		});
+	});
+});
 module.exports = router;
