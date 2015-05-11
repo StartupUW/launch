b378afb92ceb68c41d41b20bf893437699dd7b83
Daniel Fang <danfang@uw.edu>
2015-05-11 09:08:00 +0000

    React search, filter, sort done

--- public/js/projects.jsx
+++ public/js/projects.jsx
@@ -1,3 +1,43 @@
+var DEFAULT_SORT = 'top';
+
+var changeSort = function(sort) {
+    return function() {
+        this.setState({selected: sort});
+        $('#sort-method').val(sort).trigger('change');
+    }
+}
+
+var SortControl = React.createClass({
+    getInitialState: function() {
+        return {selected: DEFAULT_SORT};
+    },
+    newClick: changeSort('new'),
+    topClick: changeSort('top'),
+    render: function() {
+        var selected = this.state.selected;
+
+        var getBtn = function(match) {
+            return "btn" + (selected == match ? ' btn-primary': ' btn-default');
+        }
+
+        var newClassName  = getBtn('new');
+        var topClassName = getBtn('top');
+        return (
+            <div>
+                <p>SORT BY</p>
+                <button onClick={this.newClick} className={newClassName}>New</button>
+                <button onClick={this.topClick} className={topClassName}>Top</button>
+                <input id="sort-method" type="hidden" />
+            </div>
+        );
+    }
+});
+
+React.render(
+    <SortControl />,
+    document.getElementById('sort')
+);
+
 var ProjectImage = React.createClass({
     render: function() {
         var srcUrl = this.props.src ? this.props.src : '/img/suw-logo.png';
@@ -52,18 +92,17 @@ var VoteButton = React.createClass({
 var ProjectInfo = React.createClass({
     render: function() {
         var labelNodes = this.props.project.tags.map(function(tag) {
-            return (<span className="label label-default">{tag}</span>);
+            return (<span key={tag} className="label label-default">{tag}</span>);
         });
 
         var memberNodes = this.props.project.contact.map(function(member) {
-            return (<span className="member">{member}</span>);
+            return (<span key={member} className="member">{member}</span>);
         });
 
-        var projectUrl = "/project/" + this.props.project._id;
+        var id = this.props.project._id;
+        var projectUrl = "/project/" + id;
         var votes = this.props.project.votes;
 
-        var projectUrl = "/project/" + this.props.project._id;
-        var votes = this.props.project.votes;
         return (
             <div className="project-info col-sm-9">
                 <h2 className="project-title"> {this.props.project.name} 
@@ -78,7 +117,7 @@ var ProjectInfo = React.createClass({
                 <a href={projectUrl}>
                     <div className="more"> SEE MORE </div>
                 </a>
-                <VoteButton id={this.props.project._id} user={this.props.user} votes={this.props.project.votes} />
+                <VoteButton key={id} id={id} user={this.props.user} votes={this.props.project.votes} />
             </div>
         );
     }
@@ -107,15 +146,34 @@ var Project = React.createClass({
 
 var ProjectList = React.createClass({
     getInitialState: function() {
-        return {projects: [], user: {}};
+        return {projects: [], user: {}, filter: '', sort: DEFAULT_SORT};
     },
     componentDidMount: function() {
+        var topSort = function(a, b) { return b.votes.length - a.votes.length; };
+        var newSort = function(a, b) { 
+            var test = new Date(a.date) < new Date(b.date); 
+            return test ? -1 : 1;
+        };
         $.ajax({
             url: this.props.url,
             dataType: 'json',
             cache: false,
             success: function(data) {
-                this.setState({projects: data.projects, user: data.user});
+                this.setState({projects: data.projects.sort(topSort), user: data.user});
+                $('#project-filter').keyup(this, function(event) {
+                    event.data.setState({filter: $(this).val()});
+                });
+                $('#sort-method').change(this, function(event) {
+                    var sort = $(this).val();
+                    var sortProjects = function(a, b) {
+                        switch(sort) {
+                            case 'new': return newSort(a, b);
+                            case 'top': return topSort(a, b);
+                        }
+                        return -1;
+                    };
+                    event.data.setState({sort: sort,  projects: event.data.state.projects.sort(sortProjects) });
+                });
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(this.props.url, status, err.toString());
@@ -124,10 +182,20 @@ var ProjectList = React.createClass({
     },
     render: function() {
         var user = this.state.user;
-        var projectNodes = this.state.projects.map(function(project, index) {
+        var filter = this.state.filter.toLowerCase();
+        var sort = this.state.sort;
+        var projectNodes = this.state.projects.filter(function(project) {
+            return !filter || project.name.toLowerCase().indexOf(filter) != -1 ||
+                project.description.toLowerCase().indexOf(filter) != -1 ||
+                project.tags.join(' ').toLowerCase().indexOf(filter) != -1;
+        }).map(function(project, index) {
             return (<Project key={project._id} project={project} index={index} user={user} />);
         });
-        return (<div>{projectNodes}</div>);
+
+        if (projectNodes.length > 0) {
+            return (<div>{projectNodes}</div>);
+        } 
+        return (<div><h1>No projects found</h1></div>);
     }
 });
 
