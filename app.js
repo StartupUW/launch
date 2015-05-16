c049ae4468b5d82706879cc340f5785519931d2e
Daniel Fang <danfang@uw.edu>
2015-05-16 12:50:38 +0000

    Adding back the correct port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
-var port = 7000;
+var port = 8080;
 
 mongoose.connect('mongodb://127.0.0.1/traction');
 
